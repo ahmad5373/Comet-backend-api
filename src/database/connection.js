a5dd92adfaf52e177d73d44a2997f5d3cbf6c1a6
ahmad5373 <ahmadraxa550@gmail.com>
2024-01-17 17:21:53 +0500

    update connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -8,11 +8,17 @@ let connection;
 if (process.env.NODE_ENV === 'production') {
   connection = new Sequelize(process.env.POSTGRES_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+          ssl: process.env.NODE_ENV === 'production', // Enable SSL only in production
+        },
   });
 } else {
   connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    dialectOptions: {
+          ssl: process.env.NODE_ENV === 'production', // Enable SSL only in production
+        },
   });
 }
 // const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
