e82bf71c9bee4ef54b74b1cfbb93836048f0b504
ahmad5373 <ahmadraxa550@gmail.com>
2024-01-17 16:08:43 +0500

    update connection.js for production

--- src/database/connection.js
+++ src/database/connection.js
@@ -6,9 +6,10 @@ require('dotenv').config(); // load environment variable from env file
 const connection = new Sequelize(process.env.DB_NAME , process.env.DB_USER  , process.env.DB_PASSWORD,{
   host: process.env.DB_HOST,
   dialect: 'postgres', 
-  ssl: true, // Adjust as needed for your production database
-
-
+  logging: false, // Disable logging to reduce noise
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === 'production', // Enable SSL only in production
+  },
 });
 
 // Test the connection
