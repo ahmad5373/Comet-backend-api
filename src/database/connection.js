86280439ec7ae25065728da347ba2c4a8e61efb3
ahmad5373 <ahmadraxa550@gmail.com>
2024-01-17 15:58:05 +0500

    place dialect manually

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config(); // load environment variable from env file
 
 const connection = new Sequelize(process.env.DB_NAME , process.env.DB_USER  , process.env.DB_PASSWORD,{
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT, 
+  dialect: 'postgres', 
   ssl: true, // Adjust as needed for your production database
 
 
