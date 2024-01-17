1f3147a38beba3d2b713068d57f812b4cc0d88f5
ahmad5373 <ahmadraxa550@gmail.com>
2024-01-17 17:20:33 +0500

    add connection to production

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,26 +1,38 @@
 // database/connection.js
 const { Sequelize } = require("sequelize");
 require('dotenv').config(); // load environment variable from env file 
-const pg = require('pg'); // require 'pg' module explicitly
+// const pg = require('pg'); // require 'pg' module explicitly
 
-const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres', 
-  logging: false, // Disable logging to reduce noise
-  dialectOptions: {
-    ssl: process.env.NODE_ENV === 'production', // Enable SSL only in production
-  },
-  dialectModule: pg, // Use the explicitly required 'pg' module
-});
+let connection;
 
-// Test the connection
-connection.authenticate()
-  .then(() => {
-    console.log("Database connection established successfully.");
-  })
-  .catch((err) => {
-    console.log("Unable to connect with the database", err);
+if (process.env.NODE_ENV === 'production') {
+  connection = new Sequelize(process.env.POSTGRES_URL, {
+    dialect: 'postgres',
   });
+} else {
+  connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  });
+}
+// const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,
+//   dialect: 'postgres', 
+//   logging: false, // Disable logging to reduce noise
+//   dialectOptions: {
+//     ssl: process.env.NODE_ENV === 'production', // Enable SSL only in production
+//   },
+//   dialectModule: pg, // Use the explicitly required 'pg' module
+// });
+
+// // Test the connection
+// connection.authenticate()
+//   .then(() => {
+//     console.log("Database connection established successfully.");
+//   })
+//   .catch((err) => {
+//     console.log("Unable to connect with the database", err);
+//   });
 
 // Sync the database
 connection.sync({
